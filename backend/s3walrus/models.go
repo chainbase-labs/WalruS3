eaf2537070ac9081ee9d26f1aa2a4bc48bb51d7a
DeKaiju <longjinglv@gmail.com>
2025-08-30 00:39:32 +0800

    fix: fix bug in DeleteObject

--- backend/s3walrus/models.go
+++ backend/s3walrus/models.go
@@ -129,10 +129,16 @@ func (d *DB) CreateObject(obj *Object) error {
 
 // DeleteObject deletes an object
 func (d *DB) DeleteObject(bucketName, objectName string) error {
-	return d.db.Delete(&Object{
-		BucketName: bucketName,
-		ObjectName: objectName,
-	}).Error
+	return d.db.Transaction(func(tx *gorm.DB) error {
+		var obj Object
+		err := tx.Where("bucket_name = ? AND object_name = ?", bucketName, objectName).
+			Order("created_at DESC").
+			First(&obj).Error
+		if err != nil {
+			return err
+		}
+		return tx.Delete(&obj).Error
+	})
 }
 
 // ListObjects lists objects in a bucket with optional prefix and pagination
